2f9a8f10a2998982f3232ac92180571e40ed270e
Kevin Ong <kevinongg@gmail.comm>
2025-09-17 23:11:22 -0400

    add extras & sauces seed

---
+++ db/queries/sauces.js
@@ -0,0 +1,13 @@
+import db from "#db/client";
+
+export const createSauce = async (name, description, imageUrl, price) => {
+  const sql = `
+  INSERT INTO sauces(name, description, image_url, price) 
+  VALUES($1, $2, $3, $4) 
+  RETURNING *
+  `;
+  const {
+    rows: [sauce],
+  } = await db.query(sql, [name, description, imageUrl, price]);
+  return sauce;
+};
