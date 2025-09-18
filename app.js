2ff33a146f8af357af8663385ce9ee2eb9b1d07a
Kevin Ong <kevinongg@gmail.comm>
2025-09-18 03:18:31 -0400

    add premadebox routes

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ import cors from "cors";
 
 import userRoutes from "#api/users";
 import nigiriRoutes from "#api/nigiris";
+import preMadeBoxRoutes from "#api/preMadeBoxes";
 
 app.use(cors({ origin: process.env.CORS_ORIGIN ?? /localhost/ }));
 app.use(express.json());
@@ -18,6 +19,7 @@ app.use(getUserFromToken);
 
 app.use("/users", userRoutes);
 app.use("/nigiris", nigiriRoutes);
+app.use("/pre-made-boxes", preMadeBoxRoutes);
 
 app.route("/").get((req, res) => {
   res.send("Welcome to MizuBox API");
