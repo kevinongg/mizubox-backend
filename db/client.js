9763c48a4146ffa2d6a626ac55043baccc4f58c0
Kevin Ong <kevinongg@gmail.comm>
2025-10-08 07:00:48 -0400

    fix some code to middleware

--- db/client.js
+++ db/client.js
@@ -1,10 +1,12 @@
 import pg from "pg";
 
 /**** Use SSL if using external database ****/
-// const options = { connectionString: process.env.DATABASE_URL };
-// if (process.env.NODE_ENV === "production") {
-//   options.ssl = { rejectUnauthorized: false };
-// }
+const options = { connectionString: process.env.DATABASE_URL };
+if (process.env.NODE_ENV === "production") {
+  options.ssl = { rejectUnauthorized: false };
+}
 
-const db = new pg.Client(process.env.DATABASE_URL);
+const db = new pg.Client(options);
 export default db;
+
+
--- middleware/attachCart.js
+++ middleware/attachCart.js
@@ -3,13 +3,13 @@ import { getOrCreateCartByUserId } from "#db/queries/cart";
 const attachCart = async (req, res, next) => {
   try {
     if (!req.user || !req.user.id) {
-      return res.status(401).send("User not authenticated");
+      return res.status(401).json({ message: "User not authenticated" });
     }
     req.cart = await getOrCreateCartByUserId(req.user.id);
     return next();
   } catch (error) {
     console.error(error);
-    return res.status(404).send("Cart not found for this user");
+    return res.status(404).json({ message: "Cart not found for this user" });
   }
 };
 
